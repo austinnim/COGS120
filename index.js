19325e66f8028eb4768014db202ddbf4dc37dea8
Austin Nim <alnim@ucsd.edu>
2018-11-01 18:24:14 -0700

    added index js and css files

---
+++ index.js
@@ -0,0 +1,18 @@
+function visitLogin(){
+  window.open("./login.html");
+}
+function visitSignup(){
+  window.open("./signup.html");
+}
+
+function cardioHome(){
+  window.open("./cardio.html");
+}
+
+function powerliftingHome(){
+  window.open("./powerlifting.html");
+}
+
+function hybridHome(){
+  window.open("./hybrid.html");
+}
