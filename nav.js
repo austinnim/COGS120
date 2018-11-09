dd982787ca5e6a7408b0931bf2f789deea20fe8d
Austin Nim <alnim@ucsd.edu>
2018-11-08 19:47:18 -0800

    glossary - wizard of oz and nav bar fixed

--- nav.js
+++ nav.js
@@ -1,8 +1,8 @@
-var navbar = '<ul>\
-                  <a href="./index.html">Home</a>\
-                  <a href="./glossary.html">Glossary</a>\
-                  <a href="./community.html">Community</a>\
-                  <a href="./myprofile.html">Profile</a>\
-              </ul>';
+var navbar = '<nav>\
+                  <a href="./index.html" id="home"><img src="navbar_icons/home.png" alt="home icon" /></a>\
+                  <a href="./glossary.html" id="glossary"><img src="navbar_icons/glossary.png" alt="glossary icon" /></a>\
+                  <a href="./community.html" id="community"><img src="navbar_icons/community.png" alt="community con" /></a>\
+                  <a href="./myprofile.html" id="myprofile"><img src="navbar_icons/myprofile.png" alt="myprofile icon" /></a>\
+              </nav>';
 
 document.write(navbar);
